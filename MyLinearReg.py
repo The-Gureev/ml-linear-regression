9f46e9ad1fa45bca6be9695c8d78e4259bca495f
GureevMA <mikhail.gureev00@gmail.com>
2025-09-24 07:10:16 +0300

    Start create class

---
+++ MyLinearReg.py
@@ -0,0 +1,10 @@
+class MyLineReg:
+    def __init__(self, n_iter=100, learning_rate = 0.1):
+        self.n_iter = n_iter
+        self.learning_rate = learning_rate
+    
+    def __str__(self):
+	    return f'MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
+
+
+print(MyLineReg(10, 1))
