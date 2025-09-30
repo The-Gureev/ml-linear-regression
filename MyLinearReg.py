eaa85b58ef2e3da9ccc6cdfbd4cde4cb9c14da6b
GureevMA <mikhail.gureev00@gmail.com>
2025-09-30 22:34:43 +0300

    feat: Linear regression predict

--- MyLinearReg.py
+++ MyLinearReg.py
@@ -26,7 +26,7 @@ class MyLineReg:
         print(X_matrix.T)
         self.weights = np.ones(n_features)
 
-        for iteration in range(1, self.n_iter, 1):
+        for iteration in range(1, self.n_iter+1, 1):
             predictions = np.dot(X_matrix, self.weights)
             errors = predictions - y_vector
             gradient = (2 / len(y_vector)) * (X_matrix.T @ errors)
@@ -42,10 +42,23 @@ class MyLineReg:
             raise ValueError("Model is not fitted yet. Call fit() first.")
         return self.weights[1:]
 
+    def predict(self, features):
+        features_with_bias = features.copy()
+        features_with_bias.insert(0, 'bias', 1)
+        matrix = features_with_bias.values
+        result = np.dot(matrix, self.weights)
+        total_sum = np.sum(result)
+        return total_sum
+
+
 
 lineReg = MyLineReg(50, 0.1)
 X = pd.DataFrame({'X': [0,1,2,3,4,5,6,7,8,9]})
 
 y = pd.Series(X['X'] * 2)
-lineReg.fit(X, y, True)
-print(lineReg.get_coef())
+lineReg.fit(X, y, False)
+
+df_pd = pd.DataFrame({'X': [0,1,2,3,4,5,6,7,8,9]})
+print(df_pd)
+# print(lineReg.get_coef())
+lineReg.predict(df_pd)
