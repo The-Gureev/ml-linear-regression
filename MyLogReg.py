a5274a006e857b538b613e984b14674ae874aca7
GureevMA <mikhail.gureev00@gmail.com>
2025-10-25 22:00:06 +0300

    feat: logistic regression gradient and predict

--- MyLogReg.py
+++ MyLogReg.py
@@ -1,5 +1,6 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
+import math
 
 class MyLogReg:
     def __init__(self, n_iter = 10, learning_rate = 0.1, weights = None):
@@ -10,20 +11,48 @@ class MyLogReg:
     def __str__(self):
 	    return f'MyLogReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
     
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
     def fit(self, X, y, verbose):
-        X.insert(0, 'bias', 1)
-        X_matrix = X.copy().values
+        X_with_bias = X.copy() #.insert(1, 'bias', 1)
+        X_with_bias.insert(0, 'bias', 1)
         y_vector = y.values
 
-        l_features = len(X_matrix)
+        X_matrix = X_with_bias.values
+        n_samples = X_matrix.shape[0]
+        n_features = X_with_bias.shape[1]
+
+        self.weights = np.ones(n_features)
+       
+        eps = 1e-15
+
+        predict = self.sigmoid(X_matrix @ self.weights)
+        log_loss = -1*np.mean( y_vector * np.log(predict + eps) + (1-y_vector)*np.log(1-predict + eps) )
+
+        if verbose:
+            print(f'start | loss: {log_loss:.2f}')
+
+        for rate in range(self.n_iter):
+            predict = self.sigmoid(X_matrix @ self.weights)
+            
+            log_loss = -1*np.mean( y_vector * np.log(predict + eps) + (1-y_vector)*np.log(1-predict + eps) )
+            gradient = (X_matrix.T @ (predict - y_vector)) / n_samples
+            self.weights -= self.learning_rate * gradient
+            if verbose and rate % verbose == 0:
+                print(f'{rate} | loss: {log_loss:.2f}')
+    
+    def get_coef(self):
+        if self.weights is None:
+            raise ValueError("Model is not fitted yet. Call fit() first.")
+        return self.weights[1:]
 
 
-        for i in range(self.n_iter):
-            print(i)
+X = pd.DataFrame({'X': [0,1,2,3,4,5,6,7,8,9,10]})
+y = pd.Series([0,1,2,3,4,5,6,7,8,9,10])
 
-X = pd.DataFrame({'X':[0,1,2,3,4,5,6,7,8,9]})
-y = pd.Series([0.1,1.2,2.1,3.2,4.3,5.4,6.5,7.6,8.7,9.1])
+log_reg = MyLogReg()
+log_reg.fit(X, y, True)
 
-myLogReg = MyLogReg()
-myLogReg.fit(X, y, 10)
+
 
